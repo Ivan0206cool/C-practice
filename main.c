49e574298bedecbb06460e229161f957c93c4675
ivan-hsu <youvan0206@gmail.com>
2023-05-28 20:32:32 +0800

    verify the scanf and printf command are working

--- main.c
+++ main.c
@@ -3,17 +3,26 @@
 
 
 int main(){
-   int age;
-   char sex; 
-   printf("Enter your age:");
-   scanf("%d",&age);
-   printf("your input age is :%d\n",age);
+   int age; 
+   char name[50],sex[10];
 
+   printf("Enter your name:");  
+   scanf("%s",name);   
    fflush(stdin);    // 清除輸入緩衝區
 
-   printf("Enter your sex (B or G):");  
-   scanf("%c",&sex);   
-   printf("your input sex is :%c",sex);
+   printf("Enter your age:");
+   scanf("%d",&age);  
+   fflush(stdin);    // 清除輸入緩衝區
+
+   printf("Enter your sex :");  
+   scanf("%s",sex);   
+   
+   printf("your input sex is :%s\n",name);
+   printf("your input age is :%d\n",age);
+   printf("your input sex is :%s\n",sex);
+
+
+
 }
  
 
