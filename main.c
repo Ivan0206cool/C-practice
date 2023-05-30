76023eef51b11d1ba94d909238bc61c88a7b79bb
ivan-hsu <youvan0206@gmail.com>
2023-05-31 00:23:09 +0800

    2023/5/30 practice C language struct

--- main.c
+++ main.c
@@ -1,25 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+struct animal
+{
+   char names[10];
+   int  age;
+}dog;
+
+
 
 
 int main(){
    int age; 
    char name[50],sex[10];
 
-   printf("Enter your name:");  
-   scanf("%s",name);   
-   fflush(stdin);    // 清除輸入緩衝區
+   // printf("Enter your name:");  
+   // scanf("%s",name);   
+   // fflush(stdin);    // 清除輸入緩衝區
 
-   printf("Enter your age:");
-   scanf("%d",&age);  
-   fflush(stdin);    // 清除輸入緩衝區
+   // printf("Enter your age:");
+   // scanf("%d",&age);  
+   // fflush(stdin);    // 清除輸入緩衝區
 
-   printf("Enter your sex :");  
-   scanf("%s",sex);   
+   // printf("Enter your sex :");  
+   // scanf("%s",sex);   
    
-   printf("your input sex is :%s\n",name);
-   printf("your input age is :%d\n",age);
-   printf("your input sex is :%s\n",sex);
+   // printf("your input sex is :%s\n",name);
+   // printf("your input age is :%d\n",age);
+   // printf("your input sex is :%s\n",sex);
+
+   int *p;
+   p=(int *)(malloc(sizeof(int)));
+   printf("%d\n",&p);
+   free(p);
+
+   struct animal dog,cat;
+   strcpy(dog.names, "Tom");
+   dog.age=1;
+   printf("my dog's name  is %s\n",dog.names);
+   printf("my dog's age   is %d year old",dog.age);
+
 
 
 
